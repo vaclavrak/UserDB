bd1b97ffba517ab909d5fdc36a7e825ab45d470a
Vena <vena@hkfree.org>
2021-02-18 16:04:46 +0000

    changed DB in docker-compose, started refactor of PHP

---
+++ Dockerfile.php
@@ -0,0 +1,29 @@
+FROM php:7.2.34-fpm-stretch 
+
+RUN apt-get update && apt-get install -y git zip unzip libpq-dev libfreetype6-dev libjpeg62-turbo-dev libpng-dev 
+
+RUN docker-php-ext-configure pdo_mysql --with-pdo-mysql=mysqlnd \
+	&& docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
+	&& docker-php-ext-install pdo pdo_mysql gd zip
+
+
+RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini" && \
+    echo "max_input_vars = 3000" >> /usr/local/etc/php/conf.d/uploads.ini && \
+    echo "log_errors = On" >> /usr/local/etc/php/conf.d/log.ini && \
+    echo "error_log = /dev/stderr" >> /usr/local/etc/php/conf.d/log.ini && \
+    echo "zend.multibyte = On" >> /usr/local/etc/php/conf.d/mb.ini && \
+    echo "date.timezone = Europe/Prague" > /usr/local/etc/php/conf.d/timezone.ini
+
+RUN echo "<?php header('Location: /userdb/');" > /var/www/html/index.php
+
+RUN mkdir -p /opt/userdb/vendor
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
+
+WORKDIR /opt/userdb
+
+COPY composer.json /opt/userdb/
+COPY composer.lock /opt/userdb/
+
+RUN composer install
+
---
+++ Makefile
@@ -0,0 +1,37 @@
+# Makefile for Docker Nginx PHP Composer MySQL
+#
+
+include .env
+
+# MySQL
+
+MYSQL_DUMPS_DIR=db
+
+
+help:
+	@echo ""
+	@echo "usage: make COMMAND"
+	@echo ""
+	@echo "Commands:"
+	@echo "  mysql-restore       Restore backup of all databases"
+	@echo "  build-php           Build PHP image" 
+
+
+mysql-restore:	
+	@docker exec -i $(shell docker-compose ps -q web) php www/index.php migrations:continue --production
+	@docker exec -i $(shell docker-compose ps -q db) mysql -u"$(MYSQL_ROOT_USER)" -p"$(MYSQL_ROOT_PASSWORD)" $(MYSQL_DATABASE) < $(MYSQL_DUMPS_DIR)/dummy-data/2016-10-23-211747-new-init-dummy-data.sql
+	@docker exec -i $(shell docker-compose ps -q db) mysql -u"$(MYSQL_ROOT_USER)" -p"$(MYSQL_ROOT_PASSWORD)" $(MYSQL_DATABASE) < $(MYSQL_DUMPS_DIR)/dummy-data/2016-10-24-135743-api-klice.sql 2>/dev/null
+
+
+build-php:
+	@docker build -f Dockerfile.php . -t userdb_php:local
+
+
+restart-app:
+	@docker-compose stop
+	@docker-compose rm -f
+	@docker-compose up -d
+	@sleep 20
+	$(MAKE) mysql-restore
+	@docker-compose logs -f
+
